aeef3aae777f7ad01ef9043b0490761664d67070
 apershina <apershina@themsteam.com>
2019-12-09 21:47:51 +0700

    Change cl_csh method. passes were avaliable only for windows, now they work for mac systems.

--- Scripts/theWayFromOneRoomToAnotherOldBuilding/Script1575028302651.groovy
+++ Scripts/theWayFromOneRoomToAnotherOldBuilding/Script1575028302651.groovy
@@ -43,5 +43,14 @@ Mobile.tap(findTestObject('testcases/android.widget.ImageButton8'), 0)
 
 @com.kms.katalon.core.annotation.TearDown
 def cl_csh() {
-	Runtime.getRuntime().exec(".\\platform-tools\\adb.exe shell pm clear com.msteam.floors.nsu")
+	try {
+		Runtime.getRuntime().exec(".\\platform-tools\\adb.exe shell pm clear com.msteam.floors.nsu")
+	} catch (Exception e) {
+			System.err.print("RunTimeException")
+		}
+	try {
+		Runtime.getRuntime().exec("./platform-tools/adb.exe shell pm clear com.msteam.floors.nsu")
+	} catch (Exception e) {
+			System.err.print("RunTimeException")
+		}
 }
